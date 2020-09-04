aa76c00b0c96cc55ae472ca3e720094f6570c1d9
sss1689 <70538425+sss1689@users.noreply.github.com>
2020-09-04 18:56:43 -0500

    Update my.js

--- my.js
+++ my.js
@@ -5,7 +5,7 @@ var $ = function (id)
 
 var mainProgram = function () 
 {
-    var stringFirstName = prompt("What is your first name?");
+    var stringShyam = prompt("What is your first name?");
     if (!isNaN(stringFirstName)) {
         alert("You did not enter a valid first name, so I will call you Ash.  Hello Ash!");
     } else
